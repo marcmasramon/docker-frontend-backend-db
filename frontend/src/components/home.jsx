06d068f31dcf04989a7946f1ef5ca715667c201b
marcmasramon <marc.masramon98@gmail.com>
2024-01-28 21:53:45 +0100

    api_url

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Button, Card, CardBody, CardTitle, ListGroup, ListGroupItem, Modal, ModalBody, ModalHeader } from "reactstrap";
 import TodoForm from "./todo-form";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "http://localhost:3001/api"
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
